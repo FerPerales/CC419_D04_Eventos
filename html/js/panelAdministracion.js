74cae7b3e62b856bf8d0bf7b56d7565ee7b05d3a
Fer Perales <fernando.perales@live.com.mx>
2012-10-13 00:07:09 -0500

    Cambio dinamico de los checkbox funcionando. Fix #26

--- html/js/panelAdministracion.js
+++ html/js/panelAdministracion.js
@@ -2,7 +2,6 @@ window.onload = function(){
 	
 	var items = document.getElementsByClassName('comentario');
 	
-
 	document.getElementById("todos").onclick = function(){
 		for(i in items){
 			items[i].checked = 1;							
