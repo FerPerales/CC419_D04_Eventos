cb90a05b2c1167e52f35094d005b15b65113dabc
Fer Perales <fernando.perales@live.com.mx>
2012-10-12 23:55:06 -0500

    Eliminadas salidas a consola de prueba

--- html/js/panelAdministracion.js
+++ html/js/panelAdministracion.js
@@ -5,16 +5,14 @@ window.onload = function(){
 
 	document.getElementById("todos").onclick = function(){
 		for(i in items){
-			var itemActual = items[i];	
-			console.log(itemActual);							
+			var itemActual = items[i];							
 			itemActual.checked = 1;					
 		}
 	};
 
 	document.getElementById("ninguno").onclick = function(){
 		for(i in items){
-			var itemActual = items[i];	
-			console.log(itemActual);							
+			var itemActual = items[i];							
 			itemActual.checked = 0;					
 		}
 	};
