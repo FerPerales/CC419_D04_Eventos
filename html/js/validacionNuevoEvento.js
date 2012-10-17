9c8895079dbfdeae6ed77f7c9c5b32e59750ee2c
Fer Perales <fernando.perales@live.com.mx>
2012-10-16 21:16:01 -0500

    Agregada validacion del campo 'nombre del evento'

--- html/js/validacionNuevoEvento.js
+++ html/js/validacionNuevoEvento.js
@@ -1,1 +1,27 @@
-document.
+document.onload = function(){
+	$(function() {
+			$("#date_event").datepicker({  minDate: -1 });
+	});
+
+	
+	document.reg_event.enviar.onclick = validar;
+	
+	function validar(){
+		/* Constantes */
+		var VALOR_CAMPO_NOMBRE = 8;
+		
+		/* Variables */
+		var form = document.reg_event;
+		var correcto = true;
+		
+		/*Validacion del nombre*/
+		var valorCampo = form.nom_event.value;
+		if (valorCampo.length < VALOR_CAMPO_NOMBRE){
+			console.log("invalido");
+		}else{
+			console.log("Todo bien :3");
+		}	
+	}
+	
+	
+}();
