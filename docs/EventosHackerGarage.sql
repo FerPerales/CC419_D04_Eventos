24b2193f82deb1b3eef4764a2a5b8fae87a06cff
lord <lord@lord-M68MT-S2.(none)>
2012-11-11 17:10:38 -0600

    terminada la maquetación php y corregidos errores menores de html. Hace falta generar las páginas de manera dinámica.

--- docs/EventosHackerGarage.sql
+++ docs/EventosHackerGarage.sql
@@ -1,116 +1,68 @@
-CREATE DATABASE  IF NOT EXISTS `EventosHackerGarage` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `EventosHackerGarage`;
--- MySQL dump 10.13  Distrib 5.5.28, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: EventosHackerGarage
--- ------------------------------------------------------
--- Server version	5.5.28-0ubuntu0.12.04.2
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP SCHEMA IF EXISTS `EventosHackerGarage` ;
+CREATE SCHEMA IF NOT EXISTS `EventosHackerGarage` DEFAULT CHARACTER SET utf8 ;
+USE `EventosHackerGarage` ;
 
---
--- Table structure for table `categoria`
---
+-- -----------------------------------------------------
+-- Table `EventosHackerGarage`.`categoria`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `EventosHackerGarage`.`categoria` (
+  `idcategoria` INT NOT NULL ,
+  `categoria` VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (`idcategoria`) )
+ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `categoria`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `categoria` (
-  `idcategoria` int(11) NOT NULL AUTO_INCREMENT,
-  `categoria` varchar(45) NOT NULL,
-  PRIMARY KEY (`idcategoria`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `categoria`
---
+-- -----------------------------------------------------
+-- Table `EventosHackerGarage`.`usuario`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `EventosHackerGarage`.`usuario` (
+  `twitter` INT NOT NULL ,
+  `token_twitter` VARCHAR(50) NOT NULL ,
+  `admin` INT NOT NULL ,
+  PRIMARY KEY (`twitter`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-LOCK TABLES `categoria` WRITE;
-/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
-INSERT INTO `categoria` VALUES (1,'Conferencia'),(2,'Convivencia'),(3,'');
-/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `evento`
---
+-- -----------------------------------------------------
+-- Table `EventosHackerGarage`.`evento`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `EventosHackerGarage`.`evento` (
+  `idevento` INT NOT NULL ,
+  `creadoPor` INT NOT NULL ,
+  `nombre` VARCHAR(45) NOT NULL ,
+  `rutaFlyer` VARCHAR(45) NOT NULL ,
+  `descripcion` VARCHAR(45) NOT NULL ,
+  `precio` INT NOT NULL ,
+  `capacidad` INT NOT NULL ,
+  `fechaEvento` DATE NOT NULL ,
+  `fechaCreacion` DATE NOT NULL ,
+  `status` VARCHAR(15) NOT NULL ,
+  `categoria` INT NOT NULL ,
+  `motivo` VARCHAR(100) NULL ,
+  PRIMARY KEY (`idevento`) ,
+  CONSTRAINT `creadoPor`
+    FOREIGN KEY (`creadoPor` )
+    REFERENCES `EventosHackerGarage`.`usuario` (`twitter` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `categoria`
+    FOREIGN KEY (`categoria` )
+    REFERENCES `EventosHackerGarage`.`categoria` (`idcategoria` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `evento`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `evento` (
-  `idevento` int(11) NOT NULL,
-  `creadoPor` int(11) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `rutaFlyer` varchar(45) NOT NULL,
-  `descripcion` varchar(45) NOT NULL,
-  `precio` int(11) NOT NULL,
-  `capacidad` int(11) NOT NULL,
-  `fechaEvento` date NOT NULL,
-  `fechaCreacion` date NOT NULL,
-  `status` varchar(15) NOT NULL,
-  `categoria` int(11) NOT NULL,
-  `motivo` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`idevento`),
-  KEY `creadoPor` (`creadoPor`),
-  KEY `categoria` (`categoria`),
-  CONSTRAINT `creadoPor` FOREIGN KEY (`creadoPor`) REFERENCES `usuario` (`twitter`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `categoria` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`idcategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE INDEX `creadoPor` ON `EventosHackerGarage`.`evento` (`creadoPor` ASC) ;
 
---
--- Dumping data for table `evento`
---
+CREATE INDEX `categoria` ON `EventosHackerGarage`.`evento` (`categoria` ASC) ;
 
-LOCK TABLES `evento` WRITE;
-/*!40000 ALTER TABLE `evento` DISABLE KEYS */;
-INSERT INTO `evento` VALUES (1,1,'Hackers&Founders','../img/HF.img','1',50,50,'2012-08-30','2012-08-25','1',1,NULL);
-/*!40000 ALTER TABLE `evento` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `usuario`
---
 
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `usuario` (
-  `twitter` int(11) NOT NULL,
-  `token_twitter` varchar(50) NOT NULL,
-  `admin` int(11) NOT NULL,
-  PRIMARY KEY (`twitter`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuario`
---
-
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'1',1),(2,'2',0);
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2012-11-10 20:22:13
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
---
+++ html/php/panelAdministracion.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="keywords" lang="es" content="HackerGarage, Eventos, Programación Web" />
+		<meta name="author" content="lord" />
+		<meta name="description" content="Registro de eventos de programación en linea" />
+		<title>Eventos</title>
+		<link rel="stylesheet" type="text/css" href="../css/fondoymenu.css" />
+		<link rel="stylesheet" type="text/css" href="../css/posicionGeneral.css" />
+		<link rel="stylesheet" type="text/css" href="../css/panelAdministracion.css" />
+		<script src="../js/panelAdministracion.js"></script>
+	</head>
+
+	<body>
+		<header class="header" id="header">
+			<h1>Nuevos Eventos</h1>
+		</header>
+	<?
+		//include 'header.php';
+		include 'nav.php';
+	?>
+	<!-- 
+	Se necesitará un php que dibuje <section> y todo lo que ésta contiene. 
+	0. poner un for que dibuje 5 sections (por decir algo, 5 por página)
+	1. corregir los enlaces por php/blablabla.php 
+	2. la fecha y el "alt" se debe sacar de la columna NOMBRE de la tabla EVENTO
+	3. las rutas de las imágenes de los flyers DEBEN ESTAR EN LA BASE DE DATOS, por ejemplo en la columna FLYER de la tabla EVENTO
+	4. descripción del evento: TEXT_LIBRE
+	5. Seguir llenando los datos usando la base de datos.
+	  
+	-->
+	<article class="container">
+			<section class="formulario">
+				<form>
+					<div>
+						<div class="botonesSeleccion">
+							<button type="button" id="todos">Seleccionar todos</button>
+							<button type="button"id="ninguno">Deshacer selección</button>
+						</div>	
+						<div id="aprobar" class="botonesAprobar">
+							<button type="button">
+								<img src="../img/check.jpg" alt="Aprobar" width="15" height="15"></img>
+							</button>	
+							<button type="button">
+								<img src="../img/cross-mark.png" alt="Rechazar" width="15" height="15"></img>
+							</button>
+						</div>	
+					</div>
+					
+					<table border = "5">
+						<tr>
+							<th></th>
+							<th>ID</th>
+							<th>Nombre del Evento</th>
+							<th>Remitente</th>
+							<th>Comentarios</th>
+						</tr>
+						<tr>
+							<td>
+								<input type="checkbox" name="evento1" class="comentario" ></input>
+							</td>
+							<td>1</td>
+							<td>Evento 1</td>
+							<td>ElVacilón</td>
+							<td><input type="text" /></td>
+						</tr>
+						<tr>
+							<td>
+								<input type="checkbox" name="evento1" class="comentario" ></input>
+							</td>
+							<td>2</td>
+							<td>Evento 2</td>
+							<td>ElVacilón</td>
+							<td><input type="text" /></td>
+						</tr>
+						<tr>
+							<td>
+								<input type="checkbox" name="evento1" class="comentario" ></input>
+							</td>
+							<td>3</td>
+							<td>Evento 3</td>
+							<td>ElVacilón</td>
+							<td><input type="text"/></td>
+						</tr>
+						<tr>
+							<td>
+								<input type="checkbox" name="evento1"  class="comentario" ></input>
+							</td>
+							<td>4</td>
+							<td>Evento 4</td>
+							<td>ElVacilón</td>
+							<td><input type="text"/></td>
+						</tr>
+					</table>
+					<div class="botonesAprobar">
+						<button type="button" onclick="aprobarEventos()">
+							<img src="../img/check.jpg" alt="Aprobar" width="15" height="15"></img>
+						</button>	
+						<button type="button" onclick="desaprobarEventos()">
+							<img src="../img/cross-mark.png" alt="Rechazar" width="15" height="15"></img>
+						</button>
+					</div>	
+					<div class="clearer"></div>
+					<button type="submit" id="enviar" style="display: none;"></button>
+				</form>
+			</section>
+		</article>
+	<?	include 'footer.php'; ?>
+		
+	</body>
+</html>
