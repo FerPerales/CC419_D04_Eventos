a210a82d797fe9e1d87845f8c6859e608803d411
lord <lord@lord-M68MT-S2.(none)>
2012-11-11 19:51:53 -0600

    Se añade una base de datos más poblada y se termina la maquetación de index.php

--- docs/EventosHackerGarage.sql
+++ docs/EventosHackerGarage.sql
@@ -1,68 +1,116 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+CREATE DATABASE  IF NOT EXISTS `EventosHackerGarage` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `EventosHackerGarage`;
+-- MySQL dump 10.13  Distrib 5.5.28, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: EventosHackerGarage
+-- ------------------------------------------------------
+-- Server version	5.5.28-0ubuntu0.12.04.2
 
-DROP SCHEMA IF EXISTS `EventosHackerGarage` ;
-CREATE SCHEMA IF NOT EXISTS `EventosHackerGarage` DEFAULT CHARACTER SET utf8 ;
-USE `EventosHackerGarage` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Table `EventosHackerGarage`.`categoria`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `EventosHackerGarage`.`categoria` (
-  `idcategoria` INT NOT NULL ,
-  `categoria` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`idcategoria`) )
-ENGINE = InnoDB;
+--
+-- Table structure for table `categoria`
+--
 
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categoria` (
+  `idcategoria` int(11) NOT NULL AUTO_INCREMENT,
+  `categoria` varchar(45) NOT NULL,
+  PRIMARY KEY (`idcategoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `EventosHackerGarage`.`usuario`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `EventosHackerGarage`.`usuario` (
-  `twitter` INT NOT NULL ,
-  `token_twitter` VARCHAR(50) NOT NULL ,
-  `admin` INT NOT NULL ,
-  PRIMARY KEY (`twitter`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+--
+-- Dumping data for table `categoria`
+--
 
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+INSERT INTO `categoria` VALUES (1,'Conferencia'),(2,'Convivencia'),(3,'Curso');
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `EventosHackerGarage`.`evento`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `EventosHackerGarage`.`evento` (
-  `idevento` INT NOT NULL ,
-  `creadoPor` INT NOT NULL ,
-  `nombre` VARCHAR(45) NOT NULL ,
-  `rutaFlyer` VARCHAR(45) NOT NULL ,
-  `descripcion` VARCHAR(45) NOT NULL ,
-  `precio` INT NOT NULL ,
-  `capacidad` INT NOT NULL ,
-  `fechaEvento` DATE NOT NULL ,
-  `fechaCreacion` DATE NOT NULL ,
-  `status` VARCHAR(15) NOT NULL ,
-  `categoria` INT NOT NULL ,
-  `motivo` VARCHAR(100) NULL ,
-  PRIMARY KEY (`idevento`) ,
-  CONSTRAINT `creadoPor`
-    FOREIGN KEY (`creadoPor` )
-    REFERENCES `EventosHackerGarage`.`usuario` (`twitter` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `categoria`
-    FOREIGN KEY (`categoria` )
-    REFERENCES `EventosHackerGarage`.`categoria` (`idcategoria` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Table structure for table `evento`
+--
 
-CREATE INDEX `creadoPor` ON `EventosHackerGarage`.`evento` (`creadoPor` ASC) ;
+DROP TABLE IF EXISTS `evento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `evento` (
+  `idevento` int(11) NOT NULL,
+  `creadoPor` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `rutaFlyer` varchar(45) NOT NULL,
+  `descripcion` varchar(4500) DEFAULT NULL,
+  `precio` int(11) NOT NULL,
+  `capacidad` int(11) NOT NULL,
+  `fechaEvento` date NOT NULL,
+  `fechaCreacion` date NOT NULL,
+  `status` varchar(15) NOT NULL,
+  `categoria` int(11) NOT NULL,
+  `motivo` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`idevento`),
+  KEY `creadoPor` (`creadoPor`),
+  KEY `categoria` (`categoria`),
+  CONSTRAINT `categoria` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`idcategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `creadoPor` FOREIGN KEY (`creadoPor`) REFERENCES `usuario` (`twitter`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE INDEX `categoria` ON `EventosHackerGarage`.`evento` (`categoria` ASC) ;
+--
+-- Dumping data for table `evento`
+--
 
+LOCK TABLES `evento` WRITE;
+/*!40000 ALTER TABLE `evento` DISABLE KEYS */;
+INSERT INTO `evento` VALUES (1,1,'Hackers&Founders','img/HF.jpg','Hackers & founders es una comunidad tecnológica basada en la pregunta ¿Qué Necesitas? Conoce a tus futuros socios, solicita ayuda, aprende y emprende. Nos reunimos el último Jueves del mes.',50,50,'2012-08-30','2012-08-25','1',1,NULL),(2,1,'Super Happy Dev House','img/SHDH.jpg','No olvides registrarte aqui: http://guadalaja',0,50,'2012-09-01','2012-08-13','1',2,NULL),(3,2,'3','img/HF.jpg','333333333',33,33,'2012-08-30','2012-08-30','1',2,NULL),(4,2,'4','img/HF.jpg','444444444444444',44,44,'2012-08-30','2012-08-30','1',2,NULL);
+/*!40000 ALTER TABLE `evento` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `usuario`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `twitter` int(11) NOT NULL,
+  `token_twitter` varchar(50) NOT NULL,
+  `admin` int(11) NOT NULL,
+  PRIMARY KEY (`twitter`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'1',1),(2,'2',0);
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-11-11 19:48:37
