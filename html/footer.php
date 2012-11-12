04c97aa6af9ec90cca8f7cae87d27cbf05124096
Manuel <manuelolmedo18@gmail.com>
2012-11-12 06:48:36 -0600

    Se eliminan archivo .php de la raiz

--- html/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-			<ul>
-				<li>Jorge Avila - <a href="http://github.com/hawreghi">http://github.com/hawreghi</a></li>
-			    <li>Manuel Meza - <a href="http://github.com/slifer-x">http://github.com/slifer-x</a></li>
-			    <li>Fernando Perales - <a href="http://github.com/ferperales">http://github.com/ferperales</a></li>
-			    <li>Zahib Valenzuela - <a href="http://github.com/zahib">http://github.com/zahib</a></li>
-			</ul>
-		</footer>
--- html/navAdministrador.php
+++
@@ -1,9 +0,0 @@
-<nav class="navegacionAdministrador">
-	<ul id="menu" class="main-menu">
-		<li id="inicio"><a href="index.php">Inicio</a></li>
-		<li id="cal"><a href="calendario.php">Calendario</a></li>
-		<li id="aprobar-eventos"><a href="">Administrar eventos</a></li>
-		<li id="mis-eventos"><a href="">Mis Eventos</a></li>
-		<li id="RSS"><img src="../img/rss.png" alt="RSS" width="30" height="30" /></li>
-	</ul>
-</nav>
