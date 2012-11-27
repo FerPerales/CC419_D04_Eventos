4e35549fa32c17f6fbc07284ececa3db268d6ffd
Fer Perales <fer.perales.montoya@gmail.com>
2012-11-27 10:29:40 -0600

    Agregado evento de prueba

--- html/php/getCalendar.php
+++ html/php/getCalendar.php
@@ -1,4 +1,13 @@
 <?php
-	echo []
-
+	
+	$evento[] = array(
+		'start' => 1354302000, #30 de noviembre del 2012 a las 7:00 pm
+		'end' => 1354312800, #30 de noviembre del 2012 a las 10:00 pm
+		'title' => "Evento de prueba",
+		'url' => "http://hackergarage.mx"				
+	);
+	
+	
+	echo json_encode($evento);
+	
 ?>
