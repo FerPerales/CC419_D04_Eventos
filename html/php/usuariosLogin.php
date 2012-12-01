23fc6d1774dfa509d83d1a08f09fcd5dd5e0e1e6
lord <lord@lord-M68MT-S2.(none)>
2012-11-30 20:37:23 -0600

    log in ahora toma en cuenta las tres columnas de la tabla

--- html/php/usuariosLogin.php
+++ html/php/usuariosLogin.php
@@ -1,9 +1,33 @@
 <?
 	session_start();
-	var_dump($_REQUEST);
-	var_dump($_REQUEST["usuario"]);
-	if($_REQUEST["usuario"] == 1 ) 
-		$_SESSION['idusuario'] = 1;
-	var_dump($_SESSION);		
+	//var_dump($_REQUEST);
+	//var_dump($_REQUEST["usuario"]);
+	
+	// ir a la base de datos y llenar session con los 3 datos de la tabla de usuario
+	$usuario = $_REQUEST['usuario'];
+	//Conexion a la base de datos
+	include("bd.inc");
+	$con = new mysqli($dbhost, $dbuser, $dbpass, $db);
+	//Validar que no genere error la conexión
+	if($con -> connect_error)
+		die("Por el momento no se puede acceder al gestor de la base de datos");
+	//Crear la consulta
+	$mi_query = "select * from usuario where twitter=$usuario";
+	//Ejecutar mi consulta
+	$result = $con -> query($mi_query);
+
+	//echo '<pre>';
+	//var_dump($result);
+	//echo '</pre>';
+		
+	//Cierro la conexión
+	$con -> close();
+	
+	//meter los datos de la consulta en session
+	$datosUsuario = $result -> fetch_assoc();
+	$_SESSION["twitter"]=$datosUsuario["twitter"];
+	$_SESSION["token_twitter"]=$datosUsuario["token_twitter"];
+	$_SESSION["admin"]=$datosUsuario["admin"];
+	//var_dump($_SESSION);
 	header("Location: index.php");
-?>
+	?>
