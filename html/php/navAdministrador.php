ebc164161a49c40adf8c86dea2576ec74d81680a
lord <lord@lord-M68MT-S2.(none)>
2012-11-30 21:55:27 -0600

    ahora todos los links de la navegación funcionan

--- html/php/navAdministrador.php
+++ html/php/navAdministrador.php
@@ -2,8 +2,8 @@
 	<ul id="menu" class="main-menu">
 		<li id="inicio"><a href="index.php">Inicio</a></li>
 		<li id="cal"><a href="calendario.php">Calendario</a></li>
-		<li id="aprobar-eventos"><a href="">Administrar eventos</a></li>
-		<li id="mis-eventos"><a href="">Mis Eventos</a></li>
+		<li id="aprobar-eventos"><a href="panelAdministracion.php">Administrar eventos</a></li>
+		<li id="mis-eventos"><a href="panelMisEventos.php">Mis Eventos</a></li>
 		<li id="cerrar-sesion"><a href="cerrarSesion.php">Cerrar sesión</a></li>
 		<li id="RSS"><img src="../img/rss.png" alt="RSS" width="30" height="30" /></li>
 	</ul>
