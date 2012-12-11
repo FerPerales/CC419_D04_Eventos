484b00ae2bc91111aa471be16a89f80ad21c2199
Fer Perales <fer.perales.montoya@gmail.com>
2012-12-11 11:33:18 -0600

    Cambiadas direcciones de inicio y cierre de sesion

--- html/php/nav.php
+++ html/php/nav.php
@@ -15,7 +15,7 @@
 					<li id="inicio"><a href="index.php">Inicio</a></li>
 					<li id="cal"><a href="calendario.php">Calendario</a></li>
 					<li id="env-event"><a href="altaEventos.php">Enviar Evento</a></li>
-					<li id="inicio-sesion"><a href="login.php?authenticate=1">Iniciar sesión </a></li>
+					<li id="inicio-sesion"><a href="inicioSesion.php">Iniciar sesión </a></li>
 					<li id="RSS"><img src="../img/rss.png" alt="RSS" width="30" height="30" /></li>
 					
 				</ul>
--- html/php/navUsuarioConEventos.php
+++ html/php/navUsuarioConEventos.php
@@ -4,7 +4,7 @@
 		<li id="cal"><a href="calendario.php">Calendario</a></li>
 		<li id="aprobar-eventos"><a href="altaEventos.php">Enviar Evento</a></li>
 		<li id="mis-eventos"><a href="panelMisEventos.php">Mis Eventos</a></li>
-		<li id="cerrar-sesion"><a href="login.php?wipe=1">Cerrar sesión</a></li>
+		<li id="cerrar-sesion"><a href="cerrarSesion.php">Cerrar sesión</a></li>
 		<li id="RSS"><img src="../img/rss.png" alt="RSS" width="30" height="30" /></li>
 	</ul>
 </nav>
