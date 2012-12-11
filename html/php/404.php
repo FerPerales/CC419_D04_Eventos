9bd3f02bc04ef1bdebbf991406802bd77bd47cdf
Fer Perales <fer.perales.montoya@gmail.com>
2012-12-11 11:32:08 -0600

    Agregado archivo para error 404

---
+++ html/php/404.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<h1>Este es un feo mensaje que te indica que la página que solicitaste no ha sido encontrada. Quizá
+		algún día, podrás ver este mismo error pero con un estilo bonito</h1>
+		<h2>Mientras tanto, puedes regresar al <a href="../index.php">inicio</a></h2>
+</body>
+</html>
