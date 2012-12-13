565dd9b3675203a21881d130c75f4b99c34cab8c
lord <lord@lord-M68MT-S2.(none)>
2012-12-13 02:26:35 -0600

    calendario y Administrar Eventos tienen un contenedor que no es body

--- html/php/calendario.php
+++ html/php/calendario.php
@@ -18,42 +18,42 @@ session_start();
 	</head>
 
 	<body>
-		
-	<?
-		//include 'header.php';
-		include 'nav.php';
-	?>
-	<header>
-			<h1>Calendario</h1>
-		</header>
-	<!-- 
-	Se necesitará un php que dibuje <section> y todo lo que ésta contiene. 
-	0. poner un for que dibuje 5 sections (por decir algo, 5 por página)
-	1. corregir los enlaces por php/blablabla.php 
-	2. la fecha y el "alt" se debe sacar de la columna NOMBRE de la tabla EVENTO
-	3. las rutas de las imágenes de los flyers DEBEN ESTAR EN LA BASE DE DATOS, por ejemplo en la columna FLYER de la tabla EVENTO
-	4. descripción del evento: TEXT_LIBRE
-	5. Seguir llenando los datos usando la base de datos.
-	  
-	-->
-	<section id="calendar">
-		    <!-- inicializa calendario -->	
-			<script>
-				$(document).ready(function() {
-				    	$('#calendar').fullCalendar({
-				    		header: {
-								left: 'prev,next today',
-								center: 'title',
-								right: 'month,basicWeek,basicDay'
-								
-							},
-							editable: 'false',
-							events: 'getCalendar.php'
-				    })		
-				});
-			</script>
-		</section>
-	<?	//include 'footer.php'; ?>
-		
+		<div id="container">	
+			<?
+				//include 'header.php';
+				include 'nav.php';
+			?>
+			<header>
+					<h1>Calendario</h1>
+			</header>
+			<!-- 
+			Se necesitará un php que dibuje <section> y todo lo que ésta contiene. 
+			0. poner un for que dibuje 5 sections (por decir algo, 5 por página)
+			1. corregir los enlaces por php/blablabla.php 
+			2. la fecha y el "alt" se debe sacar de la columna NOMBRE de la tabla EVENTO
+			3. las rutas de las imágenes de los flyers DEBEN ESTAR EN LA BASE DE DATOS, por ejemplo en la columna FLYER de la tabla EVENTO
+			4. descripción del evento: TEXT_LIBRE
+			5. Seguir llenando los datos usando la base de datos.
+			  
+			-->
+			<section id="calendar">
+				    <!-- inicializa calendario -->	
+					<script>
+						$(document).ready(function() {
+						    	$('#calendar').fullCalendar({
+						    		header: {
+										left: 'prev,next today',
+										center: 'title',
+										right: 'month,basicWeek,basicDay'
+										
+									},
+									editable: 'false',
+									events: 'getCalendar.php'
+						    })		
+						});
+					</script>
+				</section>
+			<?	//include 'footer.php'; ?>
+		</div>
 	</body>
 </html>
