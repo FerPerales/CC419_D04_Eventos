befb3cfdf31879ea9e0d7a6cc0faf3618a3cd871
lord <lord@lord-M68MT-S2.(none)>
2012-12-13 01:38:47 -0600

    se vista detalle y vista blog ahora tienen un contenedor que no es body

--- html/php/index.php
+++ html/php/index.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="keywords" lang="es" content="HackerGarage, Eventos, Programación Web" />
-		<meta name="author" content="Manuel Alejandro Meza Olmedo" />
+		<meta name="author" content="lord" />
 		<meta name="description" content="Registro de eventos de programación en linea" />
 		<title>Eventos</title>
 		<link rel="stylesheet" type="text/css" href="../css/posicionGeneral.css" />
@@ -16,66 +16,68 @@
 			include 'alertsFunction.php';
 		?>
 	</head>
-
+	
 	<body>
-	
-	<?
-		include 'nav.php';
-	?>
-	<header> <h1>Blog</h1> </header>
-	<?php
-		include 'consultaEventos.php';
-	?>
-		<article class="all_event">
-	<?
-	//var_dump($datos); 
-	if ($datos != NULL)
-	{	
-		foreach($datos as $key => $value)
-		{
-			echo '<section class="event">
-					<h3><a href="vistaDetalle.php?evento=', $value["idevento"], '">', $value["fechaEvento"], ': ', 
-					html_entity_decode($value["nombre"]), '</a></h3>';
-					
-			echo '<img src="', $value["rutaFlyer"], '"  />
-					<p>', 
-					html_entity_decode(htmlentities($value["descripcion"])), 
-					'</p>
-					<div id="info-event-1" class="info">
-						<p> Cuando: <span class="place" id="cuando-1">', $value["fechaEvento"], '</span></p> ',
-						'<p class="place" >Donde: <span class="place" id="donde-1">HackerGarage, Vidrio #2188, entre Simón Bolivar y Gral. San Martín, Guadalajara.</span></p>
-						<p class="place" >Precio: $<span class="place" id="precio-1">', $value["precio"], '</span>
-						';
-				if($value["capacidad"] == -1)
-					echo 'Capacidad: <span class="place" id="capacidad-1"> Ilimitada </span>';
-				else
-					echo 	'Capacidad: <span class="place" id="capacidad-1">', $value["capacidad"], '</span> '; 
-			echo			'Categoría: <span class="place" id="categoria-1"> <a href="#">', $value["categoria"], '</a> Publicado el ', $value["fechaCreacion"], '</span> por 
-						<span class="place" id="quien-1"><a href="" >', $value["creadoPor"], '</a></span> </p>
-					</div>
-				</section>';        
+		<div id="container">
+		<?
+			include 'nav.php';
+		?>
+		<header> <h1>Blog</h1> </header>
+		<?php
+			include 'consultaEventos.php';
+		?>
+			<article class="all_event">
+		<?
+		//var_dump($datos); 
+		if ($datos != NULL)
+		{	
+			foreach($datos as $key => $value)
+			{
+				echo '<section class="event">
+						<h3><a href="vistaDetalle.php?evento=', $value["idevento"], '">', $value["fechaEvento"], ': ', 
+						html_entity_decode($value["nombre"]), '</a></h3>';
+						
+				echo '<img src="', $value["rutaFlyer"], '"  />
+						<p>', 
+						html_entity_decode(htmlentities($value["descripcion"])), 
+						'</p>
+						<div id="info-event-1" class="info">
+							<p> Cuando: <span class="place" id="cuando-1">', $value["fechaEvento"], '</span></p> ',
+							'<p class="place" >Donde: <span class="place" id="donde-1">HackerGarage, Vidrio #2188, entre Simón Bolivar y Gral. San Martín, Guadalajara.</span></p>
+							<p class="place" >Precio: $<span class="place" id="precio-1">', $value["precio"], '</span>
+							';
+					if($value["capacidad"] == -1)
+						echo 'Capacidad: <span class="place" id="capacidad-1"> Ilimitada </span>';
+					else
+						echo 	'Capacidad: <span class="place" id="capacidad-1">', $value["capacidad"], '</span> '; 
+				echo			'Categoría: <span class="place" id="categoria-1"> <a href="#">', $value["categoria"], '</a> Publicado el ', $value["fechaCreacion"], '</span> por 
+							<span class="place" id="quien-1"><a href="" >', $value["creadoPor"], '</a></span> </p>
+						</div>
+					</section>';        
+			}
 		}
-	}
-	?>
-			<div class="pre-nex">
-				<button type="button" id="previous" class="boton">&laquo; Anterior</button>
-				<button type="button" id="next" class="boton">Siguiente &raquo;</button>
-			</div>
-		</article>
+		?>
+				<div class="pre-nex">
+					<button type="button" id="previous" class="boton">&laquo; Anterior</button>
+					<button type="button" id="next" class="boton">Siguiente &raquo;</button>
+				</div>
+			</article>
+		
+		<!-- 
+		Se necesitará un php que dibuje <section> y todo lo que ésta contiene. 
+		0. poner un for que dibuje 5 sections (por decir algo, 5 por página)
+		1. corregir los enlaces por php/blablabla.php 
+		2. la fecha y el "alt" se debe sacar de la columna NOMBRE de la tabla EVENTO
+		3. las rutas de las imágenes de los flyers DEBEN ESTAR EN LA BASE DE DATOS, por ejemplo en la columna FLYER de la tabla EVENTO
+		4. descripción del evento: TEXT_LIBRE
+		5. Seguir llenando los datos usando la base de datos.
+		  
+		-->
 	
-	<!-- 
-	Se necesitará un php que dibuje <section> y todo lo que ésta contiene. 
-	0. poner un for que dibuje 5 sections (por decir algo, 5 por página)
-	1. corregir los enlaces por php/blablabla.php 
-	2. la fecha y el "alt" se debe sacar de la columna NOMBRE de la tabla EVENTO
-	3. las rutas de las imágenes de los flyers DEBEN ESTAR EN LA BASE DE DATOS, por ejemplo en la columna FLYER de la tabla EVENTO
-	4. descripción del evento: TEXT_LIBRE
-	5. Seguir llenando los datos usando la base de datos.
-	  
-	-->
-
-			
-			
-	<?	include 'footer.php'; ?>
+				
+				
+		<?	include 'footer.php'; ?>
+		</div>
 	</body>
+	
 </html>
--- html/php/vistaDetalle.php
+++ html/php/vistaDetalle.php
@@ -9,64 +9,68 @@ session_start();
 		<meta name="author" content="lord" />
 		<meta name="description" content="Registro de eventos de programación en linea" />
 		<title>Detalle de evento</title>
+		<link rel="stylesheet" type="text/css" href="../css/posicionGeneral.css" />
 		<link rel="stylesheet" type="text/css" href="../css/vistadetalle.css" />
 
 	</head>
 
 	<body>
-	<?
-		include 'header.php';
-		include 'nav.php';
-		$evento = $_GET["evento"]; 
-
-		if(isset($evento) )
-		{
+		<div id="container">
+		<?
+			include 'nav.php';
+			include 'header.php';
 			
-			include 'consultaEventoDetalle.php';
-			//$capacidad = htmlentities($capacidad, ENT_QUOTES,'UTF-8');
-			$descripcion = htmlentities($fila["descripcion"], ENT_QUOTES,'UTF-8');
-			$fechaEvento = htmlentities($fila["fechaEvento"], ENT_QUOTES,'UTF-8');
-			$idevento = htmlentities($fila["idevento"], ENT_QUOTES,'UTF-8');
-			$nombre = htmlentities($fila["nombre"], ENT_QUOTES,'UTF-8');
-			$rutaFlyer = htmlentities($fila["rutaFlyer"], ENT_QUOTES,'UTF-8');
-			$capacidad = htmlentities($fila["capacidad"], ENT_QUOTES,'UTF-8');
-			$descripcion = htmlentities($fila["descripcion"], ENT_QUOTES,'UTF-8');
-			$precio = htmlentities($fila["precio"], ENT_QUOTES,'UTF-8');
-			$creadoPor = htmlentities($fila["creadoPor"], ENT_QUOTES,'UTF-8');
-			$categoria = htmlentities($fila["categoria"], ENT_QUOTES,'UTF-8');
-			
-			echo '<article class="articulo">
-			<section class="evento">
-				<div class="detalle">
-					<h3>',$fechaEvento, ': ', html_entity_decode($nombre), '</h3>
-					<div>
-						<div class="foto">
-							<img src="', $rutaFlyer, '" alt="Evento Hackers and Founders" />
-							<p>', html_entity_decode($descripcion), '</p>
+			$evento = $_GET["evento"]; 
+	
+			if(isset($evento) )
+			{
+				
+				include 'consultaEventoDetalle.php';
+				//$capacidad = htmlentities($capacidad, ENT_QUOTES,'UTF-8');
+				$descripcion = htmlentities($fila["descripcion"], ENT_QUOTES,'UTF-8');
+				$fechaEvento = htmlentities($fila["fechaEvento"], ENT_QUOTES,'UTF-8');
+				$idevento = htmlentities($fila["idevento"], ENT_QUOTES,'UTF-8');
+				$nombre = htmlentities($fila["nombre"], ENT_QUOTES,'UTF-8');
+				$rutaFlyer = htmlentities($fila["rutaFlyer"], ENT_QUOTES,'UTF-8');
+				$capacidad = htmlentities($fila["capacidad"], ENT_QUOTES,'UTF-8');
+				$descripcion = htmlentities($fila["descripcion"], ENT_QUOTES,'UTF-8');
+				$precio = htmlentities($fila["precio"], ENT_QUOTES,'UTF-8');
+				$creadoPor = htmlentities($fila["creadoPor"], ENT_QUOTES,'UTF-8');
+				$categoria = htmlentities($fila["categoria"], ENT_QUOTES,'UTF-8');
+				
+				echo '<article class="articulo">
+				<section class="evento">
+					<div class="detalle">
+						<h3>',$fechaEvento, ': ', html_entity_decode($nombre), '</h3>
+						<div>
+							<div class="foto">
+								<img src="', $rutaFlyer, '" alt="Evento Hackers and Founders" />
+								<p>', html_entity_decode($descripcion), '</p>
+							</div>
+							
+							<p>Cuando: <span class="place" id="cuando">', $fechaEvento, '</span></p>
+							<p>Donde: <span class="place" id="donde">HackerGarage, Vidrio #2188, entre Simón Bolivar y Gral. San Martín, Guadalajara.</span></p>
+							<p class="place" id="more-inf">
+							Precio: <span class="place" id="precio">$', $precio, '</span> ';
+							if($capacidad == -1)
+								echo 'Capacidad: <span class="place" id="capacidad"> Ilimitada </span> ';
+							else
+								echo	'Capacidad: <span class="place" id="capacidad">', $capacidad, '</span> ';
+							echo		'Categoría: <span class="place" id="categoria"><a href="" >', $categoria, '</a></span> Publicado el ', $fechaEvento, ' por  
+							<span class="place" id="quien"><a href="" >', $creadoPor, '</a></span>
+						</p>
 						</div>
-						
-						<p>Cuando: <span class="place" id="cuando">', $fechaEvento, '</span></p>
-						<p>Donde: <span class="place" id="donde">HackerGarage, Vidrio #2188, entre Simón Bolivar y Gral. San Martín, Guadalajara.</span></p>
-						<p class="place" id="more-inf">
-						Precio: <span class="place" id="precio">$', $precio, '</span> ';
-						if($capacidad == -1)
-							echo 'Capacidad: <span class="place" id="capacidad"> Ilimitada </span> ';
-						else
-							echo	'Capacidad: <span class="place" id="capacidad">', $capacidad, '</span> ';
-						echo		'Categoría: <span class="place" id="categoria"><a href="" >', $categoria, '</a></span> Publicado el ', $fechaEvento, ' por  
-						<span class="place" id="quien"><a href="" >', $creadoPor, '</a></span>
-					</p>
 					</div>
-				</div>
-				<div class="info" id="event-det">
-					
-				</div>';  
-				include 'disqus.php';
-				echo 
-				'</section>
-		</article>	';
-		}
-		
-	?>
+					<div class="info" id="event-det">
+						
+					</div>';  
+					include 'disqus.php';
+					echo 
+					'</section>
+			</article>	';
+			}
+			
+		?>
+		</div>
 	</body>
 </html>
