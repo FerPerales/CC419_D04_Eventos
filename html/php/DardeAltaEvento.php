969f2391092d6239f4607d586dd91a967efc1cc0
Fer Perales <fer.perales.montoya@gmail.com>
2012-12-14 05:30:41 -0600

    Cambiado mensaje del tweet de notificacion

--- html/php/DardeAltaEvento.php
+++ html/php/DardeAltaEvento.php
@@ -142,7 +142,7 @@
 		header("LOCATION: altaEventos.php?error=9");
 	}
 	include 'enviarTweet.php';
-	$tweet = "Hola @".$_SESSION['twitter'] ."!. Tu evento esta en lista de espera para ser aprobado";
+	$tweet = "Hola @".$_SESSION['twitter'] ."!. Tu evento esta en lista de espera para ser aprobado. Saludos!";
 	$callback = post_tweet($tweet);
 	header("Location: index.php?success=1");
 ?>
