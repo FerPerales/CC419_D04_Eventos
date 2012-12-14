d1a6e3eeabc2bb639b1ea2bd8a9211d300aa8c15
Fer Perales <fer.perales.montoya@gmail.com>
2012-12-13 22:33:33 -0600

    Calendario ya obtiene los colores desde la base de datos fix #89

--- html/php/getCalendar.php
+++ html/php/getCalendar.php
@@ -1,6 +1,6 @@
 <?php
 	
-		//Conectarse a la base de datos
+	//Conectarse a la base de datos
 	require_once("bd.inc");
 	$con = new mysqli($dbhost, $dbuser, $dbpass, $db);
 	
@@ -8,47 +8,33 @@
 	if($con -> connect_error)
 		die("Por el momento no se puede acceder al gestor de la base de datos");
 
+	$mi_query = "SELECT evento.idevento, evento.nombre, evento.fechaEvento, categoria.color
+				FROM evento LEFT JOIN categoria 
+				ON evento.categoria=categoria.idcategoria 
+				WHERE status LIKE 'aprobado'"; 
 
-	//Creo la consulta
-	$mi_query = "SELECT idevento, nombre, fechaEvento, categoria
-				 FROM evento
-				 WHERE status LIKE 'aprobado'";
-	
-	//Ejecuto mi consulta
 	$result = $con -> query($mi_query);
-	
-	//Cierro la conexión
-	$con -> close();
 
-	//Convierto el resultado de mi consulta a una matriz
 	$cuantosRenglones = $result -> num_rows;
-	
-	//Creo el arreglo de arreglos donde guardaré los datos de mi evento
+
 	$eventos = array();
-	
-	$colorEvento = array(
-	    "1" => "#FF0000",
-	    "2" => "#008000",
-	    "3" => "#0000FF",   
-    );
 	
 	if($cuantosRenglones >= 1){
 		
-		//Por cada fila obtengo un arreglo		
 		while($fila = $result -> fetch_assoc())
-		
-			//Agrego los nuevos objetos que creo con la información de la base de datos	
+	
 			array_push($eventos, array(
 				'start' => strtotime($fila['fechaEvento']), 
 				'end' => strtotime($fila['fechaEvento']),
 				'title' => $fila['nombre'],
 				'url' => "vistaDetalle.php?evento=".$fila['idevento'],
-				'allDay' => true,
-				'color' => $colorEvento[$fila['categoria']]				
+				'allDay' => true,	
+				'color' => $fila['color']
 			));
 			
 	}
-	//Imprimo el arreglo en formato JSON
+
+	$con -> close();
 	echo json_encode($eventos);
 	
 ?>
