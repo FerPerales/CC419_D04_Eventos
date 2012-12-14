43917bf53d3dbc81e45414caf79c2e21f94b044a
Fer Perales <fer.perales.montoya@gmail.com>
2012-12-14 08:21:04 -0600

    Corregida la advertencia sobre preg_match() expects at least 2 parameters, 1 given

--- html/php/eventoElimitar.php
+++ html/php/eventoElimitar.php
@@ -5,7 +5,7 @@
 	//Limpiar entrada
 	$id = $_REQUEST["id"];
 	$id = htmlentities($id);
-	if(!preg_match("/[0-9]*/"))
+	if(!preg_match("/[0-9]*/", $id))
 		header("Location: ".$_SERVER["REQUEST_URI"]);
 	
 	eliminarEvento($id);
